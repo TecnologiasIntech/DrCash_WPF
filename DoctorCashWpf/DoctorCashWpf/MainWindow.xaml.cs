fb15adeae3753b17432707a4547dea2e0c750899
CarlosAlatorre <carloS_alatorrE_95@hotmail.com>
2017-07-12 13:47:13 -0700

    implementacion de ejemplo para insertar una transaccion

--- DoctorCashWpf/DoctorCashWpf/MainWindow.xaml.cs
+++ DoctorCashWpf/DoctorCashWpf/MainWindow.xaml.cs
@@ -28,31 +28,10 @@ namespace DoctorCashWpf
         private void CashInButton_Click(object sender, RoutedEventArgs e)
         {
             // Opens a new Modal Window
-            /* CashInWindow modalWindow = new CashInWindow();
-             modalWindow.ShowDialog();*/
+             CashInWindow modalWindow = new CashInWindow();
+             modalWindow.ShowDialog();
 
-            var transactionService = new transactionService();
-            var transaction = new transaction();
-            transaction.userId = 4;
-            transaction.comment = "sdfsdf";
-            transaction.type = 1;
-            transaction.amountCharged = 100;
-            transaction.cash = 100;
-            transaction.credit = 10;
-            transaction.check = 100;
-            transaction.checkNumber = 2;
-            transaction.change = 10;
-            transaction.patientFirstName = "Carlos Alatorre";
-            transaction.copayment = true;
-            transaction.selfPay = false;
-            transaction.deductible = false;
-            transaction.labs = false;
-            transaction.other = false;
-            transaction.closed = false;
-            transaction.registerId = "Asd";
-            transaction.modifiedById = 4;
-
-            transactionService.registerTransaction(transaction);
+          
         }
 
         private void CashOutButton_Click(object sender, RoutedEventArgs e)
