31b9811f876e0dc2073c692e99b8090c46e915b8
CarlosAlatorre <carloS_alatorrE_95@hotmail.com>
2017-07-19 18:42:46 -0700

    Cambio de diseño en los botones de transacciones

--- DoctorCashWpf/DoctorCashWpf/constants/constants.cs
+++ DoctorCashWpf/DoctorCashWpf/constants/constants.cs
@@ -9,7 +9,7 @@ namespace DoctorCashWpf
     class constants
     {
         // public string conectionServer = "Data Source = 192.168.1.74; Network Library=DBMSSOCN; Initial Catalog = DrCash; User ID = admin; Password = carlos123; Connection Timeout=3";
-        //public string conectionServer = "Data Source = INTECH; Initial Catalog = DrCash; User ID = admin; Password = carlos123; Connection Timeout=3";
-        public string conectionServer = "Data Source = DESKTOP-1R7B6UQ; Initial Catalog = DrCash; User ID = intech; Password = intech2017; Connection Timeout=3";
+        public string conectionServer = "Data Source = INTECH; Initial Catalog = DrCash; User ID = admin; Password = carlos123; Connection Timeout=3";
+        //public string conectionServer = "Data Source = DESKTOP-1R7B6UQ; Initial Catalog = DrCash; User ID = intech; Password = intech2017; Connection Timeout=3";
     }
 }
