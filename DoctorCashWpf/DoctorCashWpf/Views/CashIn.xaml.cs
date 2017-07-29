e7067aa8d830a5f20f37c2f3605b7e5c7a6450ac
CarlosAlatorre <carlos_alatorre_95@hotmail.com>
2017-07-29 14:02:38 -0700

    Todos los componentes de cash in funcionando

--- DoctorCashWpf/DoctorCashWpf/Views/CashIn.xaml.cs
+++ DoctorCashWpf/DoctorCashWpf/Views/CashIn.xaml.cs
@@ -24,28 +24,28 @@ namespace DoctorCashWpf
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             var transactionService = new transactionService();
             var transaction = new transaction();
 
                           transaction.userId = 4;
             //transaction.dateRegistered = DateTime.Today.ToString("d");
-            transaction.comment = comment.Text;
+            transaction.comment = txtbox_comment.Text;
                           transaction.type = 1;
 
-            transaction.amountCharged = (float)amountCharged.Value;
-            transaction.cash = (float)cash.Value;
-            transaction.credit = (float)credit.Value;
-            transaction.check = (float)check.Value;
-            transaction.checkNumber = (int)checkNumber.Value;
+            transaction.amountCharged = (float)Convert.ToDouble(txtbox_amountCharge.Text.Remove(0, 1));
+            transaction.cash = (float)Convert.ToDouble(txtbox_cash.Text.Remove(0, 1));
+            transaction.credit = (float)Convert.ToDouble(txtbox_credit.Text.Remove(0, 1));
+            transaction.check = (float)Convert.ToDouble(txtbox_check.Text.Remove(0, 1));
+            transaction.checkNumber = Convert.ToInt32(txtbox_numberChecks.Text);
                           transaction.change = 10;
-            transaction.patientFirstName = patientFirstName.Text;
-            transaction.copayment = (bool)copayment.IsChecked;
-            transaction.selfPay = (bool)selfPay.IsChecked; ;
-            transaction.deductible = (bool)deductible.IsChecked; ;
-            transaction.labs = (bool)labs.IsChecked; ;
-            transaction.other = (bool)other.IsChecked; ;
+            transaction.patientFirstName = txtbox_patientFirstName.Text;
+            transaction.copayment = (bool)checkbox_copayment.IsChecked;
+            transaction.selfPay = (bool)checkbox_selfPay.IsChecked; ;
+            transaction.deductible = (bool)checkbox_deductible.IsChecked; ;
+            transaction.labs = (bool)checkbox_labs.IsChecked; ;
+            transaction.other = (bool)checkbox_other.IsChecked; ;
                           transaction.closed = false;
                           transaction.registerId = "Asd";
                           transaction.modifiedById = 4;
@@ -55,33 +55,121 @@ namespace DoctorCashWpf
 
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+
+        private void getTotal_amount_change()
         {
-            var transactionService = new transactionService();
-            var transaction = new transaction();
+            double total = 0;
+            if(txtbox_cash.Text != "")
+            {
+                total += Convert.ToDouble(txtbox_cash.Text.Remove(0, 1));
+            }
 
-            transaction.userId = 4;
-            //transaction.dateRegistered = DateTime.Today.ToString("d");
-            transaction.comment = comment.Text;
-            transaction.type = 1;
+            if(txtbox_credit.Text != "")
+            {
+                total += Convert.ToDouble(txtbox_credit.Text.Remove(0, 1));
+            }
 
-            transaction.amountCharged = (float)amountCharged.Value;
-            transaction.cash = (float)cash.Value;
-            transaction.credit = (float)credit.Value;
-            transaction.check = (float)check.Value;
-            transaction.checkNumber = (int)checkNumber.Value;
-            transaction.change = 10;
-            transaction.patientFirstName = patientFirstName.Text;
-            transaction.copayment = (bool)copayment.IsChecked;
-            transaction.selfPay = (bool)selfPay.IsChecked; ;
-            transaction.deductible = (bool)deductible.IsChecked; ;
-            transaction.labs = (bool)labs.IsChecked; ;
-            transaction.other = (bool)other.IsChecked; ;
-            transaction.closed = false;
-            transaction.registerId = "Asd";
-            transaction.modifiedById = 4;
+            if (txtbox_check.Text != "")
+            {
+                total += Convert.ToDouble(txtbox_check.Text.Remove(0, 1));
+            }
 
-            transactionService.setTransaction(transaction);
+            label_total.Text = "$" + total.ToString() + ".00";
+
+            label_change.Text = "$" + ( total - Convert.ToDouble(txtbox_amountCharge.Text.Remove(0, 1)) ).ToString() + ".00";
+
+        }
+
+        private void txtbox_amountCharge_LostFocus(object sender, RoutedEventArgs e)
+        {
+            label_amount.Text = txtbox_amountCharge.Text;
+            label_change.Text = "$" + (Convert.ToDouble(label_total.Text.Remove(0, 1)) - Convert.ToDouble(txtbox_amountCharge.Text.Remove(0, 1))).ToString() + ".00";
+        }
+
+        private void txtbox_amountCharge_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                label_amount.Text = txtbox_amountCharge.Text;
+
+                label_change.Text = "$" + (Convert.ToDouble(label_total.Text.Remove(0, 1)) - Convert.ToDouble(txtbox_amountCharge.Text.Remove(0, 1))).ToString() + ".00";
+            }
+        }
+
+        private void txtbox_cash_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                getTotal_amount_change();
+            }
+        }
+
+        private void txtbox_cash_LostFocus(object sender, RoutedEventArgs e)
+        {
+            getTotal_amount_change();
+        }
+
+        private void txtbox_credit_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                getTotal_amount_change();
+            }
+        }
+
+        private void txtbox_credit_LostFocus(object sender, RoutedEventArgs e)
+        {
+            getTotal_amount_change();
+        }
+
+        private void txtbox_check_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                getTotal_amount_change();
+            }
+        }
+
+        private void txtbox_check_LostFocus(object sender, RoutedEventArgs e)
+        {
+            getTotal_amount_change();
+        }
+
+        private void txtbox_numberChecks_KeyUp(object sender, KeyEventArgs e)
+        {
+            
+        }
+
+        private void txtbox_numberChecks_LostFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            clearInputs();
+        }
+        
+        private void clearInputs()
+        {
+            txtbox_amountCharge.Text = "$0.00";
+            txtbox_cash.Text = "$0.00";
+            txtbox_credit.Text = "$0.00";
+            txtbox_check.Text = "$0.00";
+            txtbox_numberChecks.Text = "0";
+            txtbox_patientFirstName.Text = "";
+            txtbox_comment.Text = "";
+
+            label_amount.Text = "$0.00";
+            label_change.Text = "$0.00";
+            label_total.Text = "$0.00";
+
+            checkbox_copayment.IsChecked = false;
+            checkbox_deductible.IsChecked = false;
+            checkbox_labs.IsChecked = false;
+            checkbox_other.IsChecked = false;
+            checkbox_selfPay.IsChecked = false;
+
         }
     }
 }
