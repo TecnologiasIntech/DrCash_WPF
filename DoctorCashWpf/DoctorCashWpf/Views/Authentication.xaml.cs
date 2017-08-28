50540d29f402991017dc0538b22f10090a0f5537
EduardoRoblero <jesus.eduardo4ever@gmail.com>
2017-08-28 14:33:35 -0700

    Verificacion de Usuario en el login Campos No Vacios Datos no Encontrados

--- DoctorCashWpf/DoctorCashWpf/Views/Authentication.xaml.cs
+++ DoctorCashWpf/DoctorCashWpf/Views/Authentication.xaml.cs
@@ -26,6 +26,7 @@ namespace DoctorCashWpf.Views
         }
 
         private userService user = new userService();
+        private BrushConverter brushConverter = new BrushConverter();
 
         private void authentification()
         {
@@ -33,6 +34,7 @@ namespace DoctorCashWpf.Views
 
             if (userData != null)
             {
+                labelError.Content = "";
                 // Abrir Initial Cash
                 /* userInformation.userName = userData.usr_Username;
                  userInformation.userID = userData.usr_ID;*/
@@ -42,12 +44,34 @@ namespace DoctorCashWpf.Views
             else
             {
                 //Mensaje de error por datos incorrectos
+                //poner que no se encontro el usuario
+                labelError.Content = "        Verificar Datos"+"\n"+
+                                     "No se encontro el usuario";
             }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            authentification();
+            if (txtbox_username.Text == "")
+            {
+                txtbox_username.Focus();
+
+                txtbox_username.Background = (Brush)brushConverter.ConvertFrom("#f1c40f");
+                txtbox_username.Foreground = (Brush)brushConverter.ConvertFrom("#ffffff");
+                txtbox_username.FontWeight = FontWeights.Bold;
+            }
+            else if (txtbox_password.Password.ToString() =="")
+            {
+                txtbox_password.Focus();
+
+                txtbox_password.Background = (Brush)brushConverter.ConvertFrom("#f1c40f");
+                txtbox_password.Foreground = (Brush)brushConverter.ConvertFrom("#ffffff");
+                txtbox_password.FontWeight = FontWeights.Bold;
+            }
+            else
+            {
+                authentification();
+            }            
         }
 
         private void authentification_KeyUp(object sender, KeyEventArgs e)
