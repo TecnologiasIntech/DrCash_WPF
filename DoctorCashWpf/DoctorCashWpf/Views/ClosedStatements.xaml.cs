667a5768a469ae20f1a5946418019dfaf034c16b
EduardoRoblero <jesus.eduardo4ever@gmail.com>
2017-09-02 20:42:13 -0700

    configuracion del correo

--- DoctorCashWpf/DoctorCashWpf/Views/ClosedStatements.xaml.cs
+++ DoctorCashWpf/DoctorCashWpf/Views/ClosedStatements.xaml.cs
@@ -49,6 +49,7 @@ namespace DoctorCashWpf.Views
                 if (list[i].trn_id == dato)
                 {
                     dt.Rows.Add(list[i].trn_id, list[i].change, list[i].cash, list[i].credit, list[i].check, list[i].change);
+                    
                 }
             }
             dataGridViewClosedStatement.ItemsSource = dt.DefaultView;
