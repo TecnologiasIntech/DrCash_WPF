d6be2b275be0b3f79a874b64b16e4ec6bd9aa42e
CarlosAlatorre <carloS_alatorrE_95@hotmail.com>
2017-09-13 11:10:28 -0700

    - Crear un servicio para obtener todos los registros mediante un rango de fechas o el nombre por quien fue procesado ese registro.

--- DoctorCashWpf/DoctorCashWpf/services/logService.cs
+++ DoctorCashWpf/DoctorCashWpf/services/logService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,6 +22,49 @@ namespace DoctorCashWpf
             list.Add(createItem.ofTypeColumnsValues("log_Actions", log.log_Actions));
 
             createQuery.toInsert("Log", list);
+        }
+
+        public logObj getLogs(string proccessedBy, string fromDate, string toDate)
+        {
+            var terms = new List<valuesWhere>();
+            var obj = new logObj();
+
+            if(proccessedBy != "" && fromDate != "" && toDate != "")
+            {
+                terms.Add(createItem.ofTypeValuesWhere(true, "log_Username", proccessedBy, (int)OPERATORBOOLEAN.AND, (int)OPERATOR.EQUALITY));
+            }
+
+            if (proccessedBy != "" && fromDate == "" && toDate == "")
+            {
+                terms.Add(createItem.ofTypeValuesWhere(true, "log_Username", proccessedBy, (int)OPERATORBOOLEAN.NINGUNO, (int)OPERATOR.EQUALITY));
+            }
+
+            if (fromDate != "" && toDate != "")
+            {
+                terms.Add(createItem.ofTypeValuesWhere(true, "log_DateTime", fromDate, (int)OPERATORBOOLEAN.AND, (int)OPERATOR.GREATER_THAN_OR_EQUAL));
+                terms.Add(createItem.ofTypeValuesWhere(true, "log_DateTime", toDate, (int)OPERATORBOOLEAN.NINGUNO, (int)OPERATOR.LESS_THAN_OR_EQUAL));
+            }
+
+            if (fromDate != "" && toDate == "")
+            {
+                terms.Add(createItem.ofTypeValuesWhere(true, "log_DateTime", fromDate, (int)OPERATORBOOLEAN.NINGUNO, (int)OPERATOR.GREATER_THAN_OR_EQUAL));
+            }
+
+            obj.Datatable = createQuery.toSelectAll("Log", terms);
+
+            for (int i = 0; i < obj.Datatable.Rows.Count; i++)
+            {
+                DataRow filas = obj.Datatable.Rows[i];
+                var items = new log();
+
+                items.log_Username = Convert.ToString(filas["log_Username"]);
+                items.log_DateTime = Convert.ToString(filas["log_DateTime"]);
+                items.log_Actions = Convert.ToString(filas["log_Actions"]);
+
+                obj.list.Add(items);
+            }
+
+            return obj;
         }
     }
 }
