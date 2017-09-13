cb695467196ccde5e825049c8565c45824e89ad1
CarlosAlatorre <carloS_alatorrE_95@hotmail.com>
2017-09-13 10:44:51 -0700

    Crear un servicio para registrar LOGS

---
+++ DoctorCashWpf/DoctorCashWpf/interfaces/log.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoctorCashWpf
+{
+    public class log
+    {
+        public int log_ID;
+        public string log_Username;
+        public string log_DateTime;
+        public string log_Actions;
+    }
+}
---
+++ DoctorCashWpf/DoctorCashWpf/services/logService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoctorCashWpf
+{
+    class logService
+    {
+        private sqlQueryService createQuery = new sqlQueryService();
+        private createItemsForListService createItem = new createItemsForListService();
+        private dateService date = new dateService();
+
+        public void setLog(log log)
+        {
+            var list = new List<columnsValues>();
+
+            list.Add(createItem.ofTypeColumnsValues("log_Username", log.log_Username));
+            list.Add(createItem.ofTypeColumnsValues("log_DateTime", date.getCurrentDate()));
+            list.Add(createItem.ofTypeColumnsValues("log_Actions", log.log_Actions));
+
+            createQuery.toInsert("Log", list);
+        }
+    }
+}
