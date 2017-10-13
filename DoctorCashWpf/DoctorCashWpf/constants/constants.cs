3ac46abe64b8f5a3b02e5e138a385870e4aae6de
Carlos Alatorre <carlos_alatorre_95@hotmail.com>
2017-10-13 16:44:31 -0700

    Nueva ruta de base de datos

--- DoctorCashWpf/DoctorCashWpf/constants/constants.cs
+++ DoctorCashWpf/DoctorCashWpf/constants/constants.cs
@@ -12,6 +12,7 @@ namespace DoctorCashWpf
         //public string conectionServer = "Data Source = LEGIBLE-PC\\INTECH; Initial Catalog = DrCash; User ID = admin; Password = carlos123; Connection Timeout=3";
         //public string conectionServer = "Data Source = INTECH; Initial Catalog = DrCash; User ID = admin; Password = carlos123; Connection Timeout=3";
         //public string conectionServer = "Data Source = DESKTOP-1R7B6UQ; Initial Catalog = DrCash; User ID = intech; Password = intech2017; Connection Timeout=3";
-        public string conectionServer = "Data Source = DESKTOP-EDUARDO; Initial Catalog = DrCash; User ID = Eduardo; Password = Eduardo1; Connection Timeout=3";
+        //public string conectionServer = "Data Source = DESKTOP-EDUARDO; Initial Catalog = DrCash; User ID = Eduardo; Password = Eduardo1; Connection Timeout=3";
+        public string conectionServer = "Data Source = EKOOBS-01; Initial Catalog = DrCash; User ID = admin; Password = 123456; Connection Timeout=3";
     }
 }   
